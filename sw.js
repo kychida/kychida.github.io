504faf80b6e6c4be085d2badcef6d9374d2eb74c
kchida <kchida@healthcare.ne.jp>
2021-01-12 16:31:10 +0900

    update

--- sw.js
+++ sw.js
@@ -1,8 +1,2 @@
-if ('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/pwa/js/service-worker.js', {scope: '/pwa'})
-  .then(function(reg) {
-    console.log('登録に成功しました。');
-  }).catch(function(error) {
-    console.log('登録に失敗しました。' + error);
-  });
-}
+self.addEventListener('fetch', function(e) {
+})
