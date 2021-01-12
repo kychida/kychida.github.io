c1663cbf523cce868709f3bb682a679960401f71
kchida <kchida@healthcare.ne.jp>
2021-01-12 15:38:32 +0900

    fix javascript

--- pwa/js/sw.js
+++ pwa/js/sw.js
@@ -1,7 +1,7 @@
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('/pwa/js/service-worker.js')
   .then(function(reg) {
-    console.log('登録に成功しました。);
+    console.log('登録に成功しました。');
   }).catch(function(error) {
     console.log('登録に失敗しました。' + error);
   });
